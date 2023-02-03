0a2427d66c401ac48872150d2fafd1ba93a345ea
deadly-unicorn <linatic@pm.me>
2023-02-03 22:25:05 +0200

    progress update following
    http://bit.ly/3l6EPQ2

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,13 @@ import './globals.css'
 import { Maven_Pro } from '@next/font/google'
 const maven = Maven_Pro({
   subsets:['latin'],
+  weight:['400'],
 })
+const mavenBold = Maven_Pro({
+  subsets:['latin'],
+  weight:['500'],
+})
+
 export default function RootLayout({
   children,
 }: {
@@ -16,8 +22,39 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
+  
       <body className={maven.className}>
-        {children}
+
+        <nav className='absolute w-screen flex bg-blue-200 rounded-sm p-1 gap-x-2 overflow-hidden h-9 items-center'>
+          <div>
+            Hello world!
+          </div>
+          <div>
+            What a nice day!
+          </div>
+        </nav>
+        <div className='h-screen overflow-auto bg-gradient-to-b from-neutral-800 to-gray-900 '>
+        <main className=" flex flex-col items-center px-2 py-12">
+      
+          <div className="bg-indigo-300 p-1 rounded-md max-h-10 overflow-hidden">
+            <div className={mavenBold.className}>
+              <p className="text-yellow-200 drop-shadow-lg  ">New App In Progress...</p> 
+            </div>
+          </div>
+          <div className='max-w-md lg:max-w-lg mt-5 px-5' >
+            <div className='bg-white w-full flex justify-center rounded-sm my-2 p-1'>
+              <p>Down below is the relevant information for each page</p>
+            </div>
+
+            <div>
+              {children}
+            </div>
+          </div>
+          
+        </main>
+        </div>
+
+ 
       </body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,155 @@
+'use client'
+
+import Link from "next/link"
+import React, { useEffect,useRef,useState } from "react"
+
+
 export default function Home() {
+
+
+
+  const inputRef = useRef({
+    calories:"",
+    carbs:"",
+    fat:"",
+    protein:"",
+  });
+  const [rerender,setRerender]=useState(true);
+
+  useEffect(()=>{
+    console.log("rendered")
+  },[rerender])
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>,whichInput:string) =>{
+      if (whichInput=="calories"){
+        inputRef.current.calories = e.target.value
+      }
+      else if (whichInput=="carbs"){
+        inputRef.current.carbs = e.target.value
+      }
+      else if (whichInput=="fat"){
+        inputRef.current.fat = e.target.value
+      }
+      else if (whichInput=="protein"){
+        inputRef.current.protein = e.target.value
+      }
+    }
+
+
+
+
+
   return (
-    <main className="h-screen">
-      <div className="text-red-400">
-        Simply the best.. 
+    <>
+    <div className="min-h-[40px] bg-green-100 flex flex-col items-center p-2 gap-2">
+      <div className="border-b border-black ">
+        <p>Here we display your last [thing to add]</p>
       </div>
-      <div >
-        <p ></p>
+
+      <div className="w-full">
+        <div className="grid grid-cols-3 gap-2 border-red-200 border text-center ">
+          <div className="border-r overflow-hidden">
+            <button className="w-full"><Link href="/">Yesterday</Link></button>
+          </div>
+          <div className="overflow-hidden">
+            <button className="w-full"><Link href="/">Today</Link></button>
+          </div>
+          <div className="border-l overflow-hidden">
+            <button className="w-full"><Link href="/">Tomorrow</Link></button>
+          </div>
+        </div>
       </div>
-    </main>
+    </div>
+
+
+        <div className="grid grid-rows-4 text-center  pt-2 mt-2 bg-green-100 rounded-lg border">
+          <div>
+            <p>[Average]</p>
+          </div>
+          <div className="overflow-hidden border-t border-black py-2">
+            <div className="h-10">
+            [Calories] <br/>
+
+            {inputRef.current.calories}
+            </div>
+          </div>
+          <div className="overflow-hidden border-t border-black py-2">
+            <div className="h-10">
+            
+            [Carbs] <br/>
+            {inputRef.current.carbs}
+            </div>
+          </div>
+          <div className="overflow-hidden border-t border-black py-2">
+            <div className="h-10">
+            
+            [Fat] <br/>
+            {inputRef.current.fat}
+            </div>
+          </div>
+          <div className="overflow-hidden border-t border-black py-2">
+            <div className="h-10">
+            
+            [Protein] <br/>
+            {inputRef.current.protein}
+            </div>
+          </div>  
+        </div>
+
+        <div className="grid grid-rows-3 bg-green-100 mt-2 text-center">
+          <div className="overflow-hidden  border-t border-black py-2 flex flex-col items-center">
+            <div>
+
+            <p>[Results]</p>
+
+            <InputField label="calories" handleChange={handleInputChange}/>
+            
+            <InputField label="carbs" handleChange={handleInputChange}/>
+            
+            <InputField label="fat" handleChange={handleInputChange}/>
+
+            <InputField label="protein" handleChange={handleInputChange}/>
+
+            </div>
+
+
+            <br/>
+
+            <div className="h-10 ">
+
+              <button
+                className="bg-white mt-1 p-1 rounded-lg border-l-2 border-t border-black active:border-0"
+                onClick={()=>{
+                  setRerender(!rerender);
+                  }}>
+                Click Me
+              </button>
+            </div>
+
+          
+          </div>
+          <div className="overflow-hidden  border-t border-black py-2">
+            [Target]
+          </div>
+          <div className="overflow-hidden  border-t border-black py-2">
+            [Variance]
+          </div>
+        </div>
+    
+    </>
+
+  )
+}
+
+
+function InputField({label,handleChange}:{label:string, handleChange:(e:React.ChangeEvent<HTMLInputElement>, label:string) => void}){
+  return(
+    <>
+    <p>{label}</p>
+    <input
+    onChange={(e)=>{handleChange(e,label)}}
+    type="number"/>
+    </>
+    
   )
 }
